784f0d164adc71847321694ccd47f1b06c431b81
Corno Schraverus <corno@schraverus.com>
2026-06-21 13:35:36 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/path/escaped_path.ts
+++ typescript/lib/src/implementation/manual/transformers/path/escaped_path.ts
@@ -1,3 +1,4 @@
+import * as p_ from 'pareto-core/dist/implementation/transformer'
 import p_list_from_text from 'pareto-core/dist/implementation/refiner/specials/list_from_text'
 import p_list_build_deprecated from 'pareto-core/dist/implementation/refiner/specials/list_build_deprecated'
 import p_text_from_list from 'pareto-core/dist/implementation/transformer/specials/text_from_list'
@@ -10,10 +11,10 @@ import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schem
 
 const replace_space = ($: string): string => {
     const replace = ($: string, search: number, replace: number): d_out.List_of_Characters => p_list_build_deprecated(($i) => {
-        p_list_from_text(
+        p_.from.list(p_list_from_text(
             $,
             ($) => $
-        ).__l_map_deprecated(($) => {
+        )).map(($) => {
             if ($ === search) {
                 $i['add item'](replace)
             } else {
@@ -31,7 +32,7 @@ const replace_space = ($: string): string => {
 export const Context_path = ($: d_path.Context_Path): d_path.Context_Path => {
     return {
         'start': $.start,
-        'subpath': $.subpath.__l_map_deprecated(($) => replace_space($)),
+        'subpath': p_.from.list($.subpath).map(($) => replace_space($)),
     }
 }
 
