732f783fd892973065756ccf42aab0f8294a198a
Corno Schraverus <corno@schraverus.com>
2026-06-22 09:17:09 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/commands/write_to_file.ts
+++ typescript/lib/src/implementation/manual/commands/write_to_file.ts
@@ -6,9 +6,6 @@ import p_list_from_text from 'pareto-core/dist/implementation/refiner/specials/l
 
 import * as interface_ from "../../../interface/commands"
 
-//data types
-import * as d_write_to_file from "../../../interface/data/write_to_file"
-
 //dependencies
 import * as t_prose_2_lines from "pareto-fountain-pen/dist/implementation/manual/transformers/prose/lines"
 import * as t_path_to_path from "pareto-resources/dist/implementation/manual/transformers/unrestricted_path/unrestricted_path"
@@ -25,7 +22,14 @@ export const $$: interface_.procedures.write_to_file = p_.command_procedure(
                         : $,
                 ),
                 'data': p_temp.from.list(
-                    t_prose_2_lines.Paragraph($d.paragraph, { 'indentation': $d.generic['prose serialize'].indentation }).__l_map_deprecated(($) => $ + $d.generic['prose serialize'].newline),
+                    p_temp.from.list(
+                        t_prose_2_lines.Paragraph(
+                            $d.paragraph,
+                            {
+                                'indentation': $d.generic['prose serialize'].indentation
+                            }
+                        )
+                    ).map(($) => $ + $d.generic['prose serialize'].newline),
                 ).flatten(
                     ($) => p_list_from_text(
                         $,
