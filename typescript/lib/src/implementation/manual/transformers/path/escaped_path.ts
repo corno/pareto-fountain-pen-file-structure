414147073430ab152f584f8cd1859a9a84c7f6d1
Corno Schraverus <corno@schraverus.com>
2026-07-05 09:53:13 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/path/escaped_path.ts
+++ typescript/lib/src/implementation/manual/transformers/path/escaped_path.ts
@@ -1,4 +1,5 @@
 import * as p_ from 'pareto-core/dist/implementation/transformer'
+import * as p_i from 'pareto-core/dist/interface/transformer'
 import p_list_from_text from 'pareto-core/dist/implementation/refiner/specials/list_from_text'
 import p_list_build_deprecated from 'pareto-core/dist/implementation/refiner/specials/list_build_deprecated'
 import p_text_from_list from 'pareto-core/dist/implementation/transformer/specials/text_from_list'
@@ -6,6 +7,17 @@ import p_text_from_list from 'pareto-core/dist/implementation/transformer/specia
 //data types
 import * as d_path from "pareto-resources/dist/interface/generated/liana/schemas/fs_unrestricted_path/data"
 import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
+
+export namespace interface_ {
+    export type Context_path = p_i.Transformer<
+        d_path.Context_Path,
+        d_path.Context_Path
+    >
+    export type Node_path = p_i.Transformer<
+        d_path.Node_Path,
+        d_path.Node_Path
+    >
+}
 
 
 
@@ -31,17 +43,13 @@ const replace_space = ($: string): string => {
     )
 }
 
-export const Context_path = ($: d_path.Context_Path): d_path.Context_Path => {
-    return {
+export const Context_path: interface_.Context_path = ($) => ({
         'start': $.start,
         'subpath': p_.from.list($.subpath).map(
             ($) => replace_space($)),
-    }
-}
+    })
 
-export const Node_path = ($: d_path.Node_Path): d_path.Node_Path => {
-    return {
+export const Node_path: interface_.Node_path = ($) => ({
         'context': Context_path($.context),
         'node': replace_space($.node)
-    }
-}
+    })
