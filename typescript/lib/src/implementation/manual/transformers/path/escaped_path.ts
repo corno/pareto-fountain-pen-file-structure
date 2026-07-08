553c84bbf4bc44041bbc13c4375e51dd2515be36
Corno Schraverus <corno@schraverus.com>
2026-07-08 13:40:11 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/transformers/path/escaped_path.ts
+++ typescript/lib/src/implementation/manual/transformers/path/escaped_path.ts
@@ -1,24 +1,12 @@
 import * as p_ from 'pareto-core/implementation/transformer'
-import type * as p_i from 'pareto-core/interface/transformer'
 import p_list_from_text from 'pareto-core/implementation/refiner/specials/list_from_text'
 import p_list_build_deprecated from 'pareto-core/implementation/refiner/specials/list_build_deprecated'
 import p_text_from_list from 'pareto-core/implementation/transformer/specials/text_from_list'
 
-//data types
-import type * as d_path from "pareto-resources/interface/generated/liana/schemas/fs_unrestricted_path/data"
-import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/list_of_characters/data"
+import type * as interface_ from "../../../../interface/declarations/transformers/path/escaped_path.js"
 
-export namespace interface_ {
-    export type Context_path = p_i.Transformer<
-        d_path.Context_Path,
-        d_path.Context_Path
-    >
-    export type Node_path = p_i.Transformer<
-        d_path.Node_Path,
-        d_path.Node_Path
-    >
-}
-import * as temp_interface_ from "../../../../interface/declarations/transformers/path/escaped_path.js"
+//data types
+import type * as d_out from "pareto-fountain-pen/interface/generated/liana/schemas/list_of_characters/data"
 
 
 
