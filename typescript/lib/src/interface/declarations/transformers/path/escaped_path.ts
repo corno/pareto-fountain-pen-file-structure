5626a4b954de5348643c662908254e15c36fec69
Corno Schraverus <corno@schraverus.com>
2026-07-08 14:00:19 +0200

    pdt: ...

--- typescript/lib/src/interface/declarations/transformers/path/escaped_path.ts
+++ typescript/lib/src/interface/declarations/transformers/path/escaped_path.ts
@@ -5,12 +5,12 @@ import type * as p_ from 'pareto-core/interface/transformer'
 import type * as d_path from "pareto-resources/interface/generated/liana/schemas/fs_unrestricted_path/data"
 
 
-    export type Context_path = p_.Transformer<
-        d_path.Context_Path,
-        d_path.Context_Path
-    >
-    export type Node_path = p_.Transformer<
-        d_path.Node_Path,
-        d_path.Node_Path
-    >
+export type Context_path = p_.Transformer<
+    d_path.Context_Path,
+    d_path.Context_Path
+>
+export type Node_path = p_.Transformer<
+    d_path.Node_Path,
+    d_path.Node_Path
+>
 
